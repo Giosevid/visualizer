b9708d7d9ccb2660569eba0f3f43679ac92e2b33
Giosevid Acosta <giosevid@gmail.com>
2023-04-15 00:15:09 +0200

    fix styles

--- src/components/backGroundImage.js
+++ src/components/backGroundImage.js
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 const BackGroundImage = ({ img }) => {
   return (
-    <div className="base-img">
-      <img src={img} alt="Picture of kitchen" layout="fill" objectFit="cover" />
+    <div className="base-image">
+      <img src={img} alt="Picture of kitchen" className="w-full" />
     </div>
   );
 };
