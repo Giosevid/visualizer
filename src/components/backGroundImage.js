417540bff22df50c9c0ab344ab770907599521b2
Giosevid <giosevid@gmail.com>
2023-04-17 13:50:12 +0200

    Develop (#3)
    * fix slint
    
    * fix linter
    
    * delete babel-parser
    
    * fixes to upload project to vercel
    
    * Fix eslint
    
    * fix styes

--- src/components/backGroundImage.js
+++ src/components/backGroundImage.js
@@ -1,8 +1,8 @@
 const BackGroundImage = ({ img, img2 }) => {
   return (
     <div className='relative w-full h-full'>
-      <img src={img} alt='Picture of kitchen' />
-      {img2 && <img src={img2} alt='Picture of kitchen' className='absolute top-0' />}
+      <img src={img} alt='Picture of kitchen' className="object-cover w-full h-full" />
+      {img2 && <img src={img2} alt='Picture of kitchen' className='absolute top-0 object-cover w-full h-full' />}
     </div>
   )
 }
